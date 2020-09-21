345cc9fb81123ad69a3a9d4d0d01292ec8560b60
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-09-20 23:18:43 -0300

    Testando autenticação

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,15 @@
 import AppError from '@shared/errors/AppErros';
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
+    const fakeHashProvider = new FakeHashProvider();
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -17,8 +21,10 @@ describe('CreateUser', () => {
   });
 
   it('should not be able to create a new user with same email from another', async () => {
+    const fakeHashProvider = new FakeHashProvider();
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
 
     await createUser.execute({
       name: 'John Doe',
