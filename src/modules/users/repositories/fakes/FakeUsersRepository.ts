72717f4627862a469ae10eb52942c30cf2ccc45d
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-09-20 22:48:49 -0300

    Testando criaçao do usuario

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,44 @@
+import { uuid } from 'uuidv4';
+
+import IUsersRepostory from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../../infra/typeorm/entities/User';
+
+// SOLID - L = Liskov Substitution Principle
+
+class UsersRepository implements IUsersRepostory {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.id === id);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.email === email);
+
+    return user;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UsersRepository;
