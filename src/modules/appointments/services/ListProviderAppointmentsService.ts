c08363a5e12fc772042ea1b2cbc5ef3e0344ea65
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-10-03 13:25:14 -0300

    Configurando cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,8 @@ import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError'
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 interface IRequest {
@@ -17,6 +19,9 @@ export default class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -25,12 +30,20 @@ export default class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-      provider_id,
-      year,
-      month,
-      day,
-    });
+    const cacheData = await this.cacheProvider.recover('asd');
+
+    console.log(cacheData);
+
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      }
+    );
+
+    // await this.cacheProvider.save('asd', 'asd');
 
     return appointments;
   }
---
+++ src/shared/container/providers/CacheProvider/index.ts
@@ -0,0 +1,15 @@
+import { container } from 'tsyringe';
+
+import ICashProvider from './models/ICacheProvider';
+
+import RedisCacheProvider from './implementations/RedisCacheProvider';
+
+const providers = {
+  redis: RedisCacheProvider,
+};
+
+container.registerSingleton<ICashProvider>(
+  'CacheProvider',
+  providers.redis,
+);
+
