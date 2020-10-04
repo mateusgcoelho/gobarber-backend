78ddb68d87c045840187624373f73ca9aa13c279
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-10-04 14:11:09 -0300

    Express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import uploadConfig from '@config/upload';
 
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import cors from 'cors';
 
 import { errors } from 'celebrate';
@@ -17,6 +18,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolders));
