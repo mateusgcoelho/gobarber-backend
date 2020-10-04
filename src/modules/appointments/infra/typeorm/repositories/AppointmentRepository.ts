96945f33ddc06e0e895d1c19fad08452bfc7f63b
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-10-04 16:20:30 -0300

    Clientes dos agendamentos

--- src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -61,6 +61,7 @@ class AppointmentsRepository implements IAppointmentRepostory {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
